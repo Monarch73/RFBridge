502d4029df82174aa624005a67ed46dd785f95ab
niels huesken <niels@monarch.de>
2017-10-06 14:41:43 +0000

    Dockerfile

---
+++ Docker/shell.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y build-essential autoconf git python2.7
+
+mkdir /esp
+cd /esp
+git clone https://github.com/sui77/rc-switch.git
+git clone https://github.com/me-no-dev/ESPAsyncUDP.git
+git clone https://github.com/me-no-dev/ESPAsyncWebServer.git
+git clone https://github.com/me-no-dev/ESPAsyncTCP.git
+git clone https://github.com/Monarch73/RFBridge.git
+git clone https://github.com/plerup/makeEspArduino.git
+
+cd Arduino/tools
+python2.7 ./get.py
+
+make -f /esp/makeEspArduino/makeEspArduino.mk ESP_ROOT=/esp/Arduino/ BOARD=d1_mini SKETCH=/esp/RFBridge/RFBridge/RFBridge/RFBridge.ino LIBS="/esp/Arduino/./libraries/ESP8266WiFi/src/ /esp/Arduino/./libraries/EEPROM/ /esp/ESPAsyncTCP/src/ /esp/ESPAsyncUDP/src/ /esp/ESPAsyncWebServer/src/ /esp/Arduino/libraries/Hash/src/ /esp/rc-switch/ /esp/Arduino/libraries/ESP8266mDNS/ /esp/Arduino/libraries/"

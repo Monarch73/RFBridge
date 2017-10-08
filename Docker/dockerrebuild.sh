e7e2d3bba423f3e802d971fcb6843236189e2fbd
Niels Hüsken <monarch@hotmail.de>
2017-10-08 12:44:06 +0200

    testing docker builds

--- Docker/dockerrebuild.sh
+++ Docker/dockerrebuild.sh
@@ -1,6 +1,5 @@
-docker build -t rf .
 id=$(docker run -d -t rf) 
-docker cp ../RFBridge/RFBridge/.  /esp/RFBridge/RFBridge/RFBridge/
-docker exec /rebuild.sh
+docker cp ../RFBridge/RFBridge/.  $id:/esp/RFBridge/RFBridge/RFBridge/
+docker exec $id /rebuild.sh
 docker cp $id://tmp/mkESP/RFBridge_d1_mini/RFBridge.bin ./rfbridge.bin
 docker stop $id
--- Docker/rebuild.sh
+++ Docker/rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+rm -rf /tmp/mkESP
 make -f /esp/makeEspArduino/makeEspArduino.mk ESP_ROOT=/esp/Arduino/ BOARD=d1_mini SKETCH=/esp/RFBridge/RFBridge/RFBridge/RFBridge.ino LIBS="/esp/Arduino/./libraries/ESP8266WiFi/src/ /esp/Arduino/./libraries/EEPROM/ /esp/ESPAsyncTCP/src/ /esp/ESPAsyncUDP/src/ /esp/ESPAsyncWebServer/src/ /esp/Arduino/libraries/Hash/src/ /esp/rc-switch/ /esp/Arduino/libraries/ESP8266mDNS/ /esp/Arduino/libraries/"
